caf19a50d851e9e08f6381a2745e25a794e45edb
floklausner <klausnerflo@gmail.com>
2022-09-13 16:39:04 +0200

    Klausner: added DebtorResource methods

--- src/main/java/at/htl/boundary/DebtorResource.java
+++ src/main/java/at/htl/boundary/DebtorResource.java
@@ -1,4 +1,35 @@
 package at.htl.boundary;
 
+import at.htl.control.DebtorRepository;
+import at.htl.entity.Debtor;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/debtor")
+@Produces(MediaType.APPLICATION_JSON)
 public class DebtorResource {
+
+    @Inject
+    DebtorRepository debtorRepository;
+
+    @GET
+    public Response getAllDebtors() {
+        return Response.ok(debtorRepository.findAll().list()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response findDebtorById(@PathParam("id") long id) {
+        return Response.ok(debtorRepository.findById(id)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveDebtor(Debtor debtor) {
+        return Response.ok(debtorRepository.save(debtor)).build();
+    }
+
 }
