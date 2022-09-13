618c9182cd1672b99586e0d2aa0127502e7cc02d
floklausner <klausnerflo@gmail.com>
2022-09-13 16:47:53 +0200

    Klausner: added EntryResource Methods

--- src/main/java/at/htl/boundary/EntryResource.java
+++ src/main/java/at/htl/boundary/EntryResource.java
@@ -1,4 +1,34 @@
 package at.htl.boundary;
 
+import at.htl.control.EntryRepository;
+import at.htl.entity.Entry;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/entry")
 public class EntryResource {
+
+    @Inject
+    EntryRepository entryRepository;
+
+    @GET
+    public Response findAllEntries() {
+        return Response.ok(entryRepository.findAll().list()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response findEntryById(@PathParam("id") long id) {
+        return Response.ok(entryRepository.findById(id)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveEntry(Entry entry) {
+        return Response.ok(entryRepository.save(entry)).build();
+    }
+
 }
